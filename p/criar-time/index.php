af7221189e7601706fc2d0d86446041a0a3d5e41
weu-souza <weuller1561@gmail.com>
2025-09-16 11:49:41 -0300

    leout criar time terminado

--- p/criar-time/index.php
+++ p/criar-time/index.php
@@ -8,10 +8,62 @@
 
 <body>
     <?php include '../../global/php/header/header.php'; ?>
-    <main>
+    <main class="criar-time-container">
+        <form class="formulario-criar-time">
+
+            <label for="" class="label-formulario">
+                <span class="navegacao cinza-escuro-text">Nome do time</span>
+                <input type="text" placeholder="digite o nome para seu time" name="nomeCompeticao" class="paragrafo cinza-escuro-text input-formulario">
+            </label>
+            <label for="" class="label-formulario">
+                <span class="navegacao cinza-escuro-text">Quantidade de jogadores</span>
+                <input type="number" placeholder="digite a quantidade de jogadores" name="nomeCompeticao" class="paragrafo cinza-escuro-text input-formulario">
+            </label>
+            <!-- imagem -->
+            <div class="foto">
+                <label for="formFile" id="span_imagem" class="form-label img-label">
+                    <div id="img">
+                        <span class="navegacao cinza-escuro-text">Escolha uma imagem</span>
+                    </div>
+                </label>
+                <input type="file" accept="image/*" name="foto" id="formFile">
+            </div>
+
+
+            <div class="btn-criar-time-container">
+                <button type="submit" class="button-full navegacao ">Cadastrar</button>
+            </div>
+        </form>
 
     </main>
 
 </body>
+<script>
+    //image
+    let base64String = '';
+    let file = ''
+    $('#formFile').on('change', function(event) {
+        file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+
+            reader.addEventListener('load', (e) => {
+                base64String = e.target.result;
+
+                $('#span_imagem').css({
+                    'border': 'none',
+                    'background': 'none'
+                });
+                $('#img').html(`<img src="${base64String}" class="img">`);
+            });
+
+            reader.readAsDataURL(file);
+        } else {
+            console.log('Nenhum arquivo selecionado.');
+            $('#img').html('<span class="navegacaos">Escolha uma imagem</span>');
+        }
+
+    });
+</script>
 
 </html>
