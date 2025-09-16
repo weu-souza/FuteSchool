ac1e15e5f6745744747eec1df4d1a0bdecdb488a
weu-souza <weuller1561@gmail.com>
2025-09-16 12:29:30 -0300

    formulario de criar time

--- p/inscrever-time/index.php
+++ p/inscrever-time/index.php
@@ -1,13 +1,45 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-   <title>Historico</title>
+    <title>Historico</title>
     <?php include '../../global/php/head.php'; ?>
 </head>
 
 <body>
     <?php include '../../global/php/header/header.php'; ?>
-    <main>
-    
+    <main class="criar-time-container">
+        <form class="formulario-criar-time">
+
+            <label for="" class="label-formulario">
+                <span class="navegacao cinza-escuro-text">Nome do time</span>
+                <input type="text" placeholder="digite o nome para seu time" name="nomeCompeticao" class="paragrafo cinza-escuro-text input-formulario">
+            </label>
+            <label for="" class="label-formulario">
+                <span class="navegacao cinza-escuro-text">Numero da camisa</span>
+                <input type="number" placeholder="Digite o número da sua camisa" name="nomeCompeticao" class="paragrafo cinza-escuro-text input-formulario">
+            </label>
+
+            <label for="" class="label-formulario">
+                <span class="navegacao cinza-escuro-text">Posição no time</span>
+                <input type="text" placeholder="digite sua posição no time" name="nomeCompeticao" class="paragrafo cinza-escuro-text input-formulario">
+            </label>
+            <label for="" class="label-formulario">
+                <span class="navegacao cinza-escuro-text">Titular ou reserva</span>
+                <select class="paragrafo cinza-escuro-text input-formulario">
+                    <option value="titular">Escolha uma opção</option>
+                    <option value="titular">Titular</option>
+                    <option value="reserva">Reserva</option>
+                </select>
+            </label>
+
+            <div class="btn-criar-time-container">
+                <button type="submit" class="button-full navegacao ">Registrar</button>
+            </div>
+        </form>
+
+    </main>
+
 </body>
+
 </html>
